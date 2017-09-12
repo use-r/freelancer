0d7e9ae42a1cae81797ec7c65a0dbbd4812baf31
harupy <hkawamura0130@gmail.com>
2017-09-12 21:23:20 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,11 +5,12 @@ from PIL import ImageDraw
 img_height = 800
 img_width = 600
 txt_height = 40
+txt = 'Sample Text'
 
 img = Image.new('RGB', (img_width, img_height))
 draw = ImageDraw.Draw(img)
 # font = ImageFont.truetype(<font-file>, <font-size>)
 font = ImageFont.truetype("Amble-Bold.ttf", txt_height)
-# draw.text((x, y),"Sample Text",(r,g,b))
-draw.text((0, 0),"Sample Text",(255,255,255),font=font)
+# draw.text((x, y),"your text",(r,g,b))
+draw.text((0, 0),txt,(255,255,255),font=font)
 img.save('sample-out.jpg')
